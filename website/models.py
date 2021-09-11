29e38b9ec161fcf7580fb802704bfaffb1921a1a
steve <muemasnyamai@gmail.com>
2021-09-11 17:55:26 +0300

    created user data models and a test to the sentence generator

--- website/models.py
+++ website/models.py
@@ -10,5 +10,16 @@ class User(models.Model):
         db_table = "Emails"
 
 
+class UserData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    target_industry = models.CharField(max_length=30, null=False, blank=False)
+    target_product = models.CharField(max_length=30, null=False, blank=False)
+
+
 class Sentences(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    subject = models.CharField(max_length=30, null=False, blank=False)
+    object = models.CharField(max_length=30, null=False, blank=False)
+    verb = models.CharField(max_length=30, null=False, blank=False)
+    adjective = models.CharField(max_length=30, null=False, blank=False)
+    sentence = models.TextField(max_length=30)
--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-import views as v
+from django.urls import path
+
+from website import views
 
 urlpatterns = [
-    path('', v.index),
+    path('', views.index),
 ]
