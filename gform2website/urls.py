3667fe27ec2982c2ffcf901d8c980612573ad2ad
steve <muemasnyamai@gmail.com>
2021-09-23 13:52:25 +0300

    added static files to url

--- gform2website/urls.py
+++ gform2website/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
+from gform2website import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
 ]
+urlpatterns += url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATICFILES_DIRS}),
+
